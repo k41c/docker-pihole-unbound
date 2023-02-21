b0de9002ad7899047589688716ac5d1d65b60a9e
k41c <51283438+k41c@users.noreply.github.com>
2023-02-21 20:53:37 +0100

    Update update.sh

--- one-container/update.sh
+++ one-container/update.sh
@@ -1,1 +1,7 @@
-
+docker build -t pihole-unbound-local ./pihole-unbound
+#Container herunterfahren
+docker-compose pull
+#Update durchführen
+docker-compose down
+#Container starten
+docker-compose up -d
